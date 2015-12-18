1b352ce03769348fcacd7e9e46aa4ed7cbf1641a
Alan Gutierrez <alan@prettyrobots.com>
2015-12-18 12:57:37 -0500

    Implement timeouts.

--- cliffhanger.js
+++ cliffhanger.js
@@ -1,5 +1,6 @@
 var Cache = require('magazine')
 var Monotonic = require('monotonic')
+var interrupt = require('interrupt').createInterrupter('bigeasy.cliffhanger')
 
 function Cliffhanger (cache) {
     cache = cache || new Cache
@@ -16,7 +17,18 @@ Cliffhanger.prototype.invoke = function (callback) {
 Cliffhanger.prototype.resolve = function (cookie, vargs) {
     var cartridge = this._magazine.hold(cookie, {})
     cartridge.value.callback.apply(null, vargs)
-    cartridge.release()
+    cartridge.remove()
+}
+
+Cliffhanger.prototype.expire = function (expired) {
+    var purge = this._magazine.purge()
+    while (purge.cartridge && purge.cartridge.when < expired) {
+        var error = interrupt(new Error('expired'))
+        purge.cartridge.value.callback.call(null, error)
+        purge.cartridge.remove()
+        purge.next()
+    }
+    purge.release()
 }
 
 module.exports = Cliffhanger
--- t/cliffhanger/cliffhanger.t.js
+++ t/cliffhanger/cliffhanger.t.js
@@ -1,12 +1,17 @@
-require('proof')(1, prove)
+require('proof')(2, prove)
 
 function prove (assert) {
     var Cache = require('magazine')
     var Cliffhanger = require('../..')
     new Cliffhanger
-    var cliffhanger = new Cliffhanger(new Cache)
+    var now = 0
+    var cliffhanger = new Cliffhanger(new Cache({ Date: { now: function () { return now } } }))
     var cookie = cliffhanger.invoke(function (error, result) {
         assert(result, 1, 'called back')
     })
     cliffhanger.resolve(cookie, [ null, 1 ])
+    cliffhanger.invoke(function (error, result) {
+        assert(error.message, 'expired', 'timed out')
+    })
+    cliffhanger.expire(1)
 }
