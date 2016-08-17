9f35469b3e52b536b3e7ef05b526645a7fce6bc6
Alan Gutierrez <alan@prettyrobots.com>
2016-08-17 15:49:28 -0500

    Upgrade Monotonic to 2.0.x.

--- cliffhanger.js
+++ cliffhanger.js
@@ -1,15 +1,15 @@
 var Cache = require('magazine')
-var Monotonic = require('monotonic')
+var Monotonic = require('monotonic').asString
 var interrupt = require('interrupt').createInterrupter('bigeasy.cliffhanger')
 
 function Cliffhanger (cache) {
     cache = cache || new Cache
     this._magazine = cache.createMagazine()
-    this._cookie = Monotonic.parse('0')
+    this._cookie = '0'
 }
 
 Cliffhanger.prototype.invoke = function (callback) {
-    var cookie = Monotonic.toString(this._cookie = Monotonic.increment(this._cookie))
+    var cookie = this._cookie = Monotonic.increment(this._cookie, 0)
     this._magazine.hold(cookie, { cookie: cookie, callback: callback }).release()
     return cookie
 }
